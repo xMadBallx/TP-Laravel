b1fa268da4b6869aed7d191e689765b298bf8585
Laurent Joly <loran62@orange.fr>
2022-02-28 09:29:48 +0100

    création des routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/admin-list', function () {
+    return view('admin');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+Route::get('/category', function () {
+    return view('category');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashbord');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/product', function () {
+    return view('product');
+});
+
+Route::get('/products', function () {
+    return view('products');
+});
