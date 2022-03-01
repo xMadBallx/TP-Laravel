20c01ec8445f6b0f3d91bc552e181d05830ccd66
Laurent Joly <loran62@orange.fr>
2022-03-01 12:16:03 +0100

    màj products_table

--- database/migrations/2022_02_28_141535_create_products_table.php
+++ database/migrations/2022_02_28_141535_create_products_table.php
@@ -16,6 +16,15 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('price');
+            $table->boolean('heart');
+            $table->text('colors');
+            $table->text('slug');
+            $table->string('image');
+            $table->integer('promotion');
+
         });
     }
 
